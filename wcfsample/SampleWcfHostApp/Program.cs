de867579b93c004e87d820b81cf9680bbdc27470
Nicolai Schönberg <nicolai.schoenberg@gmail.com>
2014-11-01 16:28:23 +0100

    moved content to new subdir

---
+++ wcfsample/SampleWcfHostApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace SampleWcfHostApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Sample WCF Service";
+
+            using (var host = new ServiceHost(typeof (SampleWcfServiceLibrary.SampleWcfService)))
+            {
+                // ABC Prinzip
+
+                host.AddServiceEndpoint(
+                    typeof (SampleWcfServiceLibrary.ISampleContract), // C - Contract
+                    new NetTcpBinding(), // B - Binding
+                    "net.tcp://localhost:8888/SampleEndpoint" // A - Address
+                    );
+
+                host.Open();
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
