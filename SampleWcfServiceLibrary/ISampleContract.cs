8059c4e25e183dafdf7473bd3e4ed194d10e5675
Nico <nicolai.schoenberg@gmail.com>
2014-11-01 15:44:29 +0100

    Initial commit

---
+++ SampleWcfServiceLibrary/ISampleContract.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+namespace SampleWcfServiceLibrary
+{
+    
+    [ServiceContract(Namespace = "http://blog.grrbrr.de/samples/c/2014/11/wcf")]
+    public interface ISampleContract
+    {
+        [OperationContract]
+        string Say(string msg);
+    }
+}
